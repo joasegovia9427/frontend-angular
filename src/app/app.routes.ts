bdfe54c90e34f8562c781e26b1d59e3e573969ba
Joaquin Segovia <joasegovia9427@gmail.com>
2025-12-11 11:10:59 -0300

    chore: adding nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { CountryList } from './pages/country-list/country-list';
+import { ROUTES_PATHS } from './core/models/routes';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'country', component: CountryList },
+  { path: ROUTES_PATHS.HOME, component: HomePage },
+  { path: ROUTES_PATHS.COUNTRY, component: CountryList },
 ];
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NgClass],
+  imports: [NgClass, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
